9b85f1c879fca4a2a08c625ddaa3e0c5f2bf9977
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-04-26 09:07:57 -0500

    commit tree 1c28c1f0276ed75c504053c885c3702f688be08c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arr) => {
+  let i = 0;
+  while (i < arr.length) {
+    arr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+    i++;
+  }
+  return arr;
+}
+
+const countDown = () => {
+  for (let i = 0; i<11; i++) {
+    console.log(i)
+  }
+}
